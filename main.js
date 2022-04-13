2c20e61b177234ea7ce79fa50274e4de185a5849
vlad <vladislavkulikov21@gmail.com>
2022-04-13 21:59:20 +0300

    initialcommit

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ btn1.onclick = function() {
     clicks = clicks - 1;
     btn_clicks = clicks % 3
     if(btn_clicks == 0){
-        backgroundPic.style.backgroundImage = "url('/pictures/slider_picture1.jpg')"; 
+        backgroundPic.style.backgroundImage = "url('pictures/slider_picture1.jpg')"; 
     }
     if(btn_clicks == -1 || btn_clicks == 2 ){
-        backgroundPic.style.backgroundImage = "url('/pictures/slider_picture3.jpg')"; 
+        backgroundPic.style.backgroundImage = "url('pictures/slider_picture3.jpg')"; 
     }
     if(btn_clicks == -2 || btn_clicks == 1){
-        backgroundPic.style.backgroundImage = "url('/pictures/slider_picture2.jpg')"; 
+        backgroundPic.style.backgroundImage = "url('pictures/slider_picture2.jpg')"; 
     }
 };
 
@@ -23,19 +23,19 @@ btn2.onclick = function() {
     clicks = clicks + 1;
     btn_clicks = clicks % 3
     if(btn_clicks === 0){
-        backgroundPic.style.backgroundImage = "url('/pictures/slider_picture1.jpg')"; 
+        backgroundPic.style.backgroundImage = "url('pictures/slider_picture1.jpg')"; 
     }
     if(btn_clicks === -1 || btn_clicks === 2 ){
-        backgroundPic.style.backgroundImage = "url('/pictures/slider_picture3.jpg')"; 
+        backgroundPic.style.backgroundImage = "url('pictures/slider_picture3.jpg')"; 
     }
     if(btn_clicks === -2 || btn_clicks === 1){
-        backgroundPic.style.backgroundImage = "url('/pictures/slider_picture2.jpg')"; 
+        backgroundPic.style.backgroundImage = "url('pictures/slider_picture2.jpg')"; 
     }
 };
 
 let backToTop = document.querySelector('.toTop');
 
 backToTop.onclick = function(){
-    document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 } 
